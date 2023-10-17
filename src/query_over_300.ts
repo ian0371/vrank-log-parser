e1771ab1f85063ee116fa29b5a46ea465b316f08
Chihyun Song <ian0371@gmail.com>
2023-10-17 12:25:06 +0900

    Add proposer, logger to query_over_300

--- src/query_over_300.ts
+++ src/query_over_300.ts
@@ -1,49 +1,48 @@
 import * as mongoose from "mongoose";
-import { VrankLog, VrankLogMetadata } from "./schema";
+import { VrankLog } from "./schema";
 
 async function main() {
   console.log("Connecting Mongo DB...");
   await mongoose.connect("mongodb://127.0.0.1:27017/vrank");
   console.log("Connected successfully");
 
-  const threshold = 0;
-  const metadata = await VrankLogMetadata.findOne();
-  if (metadata == null) {
-    throw Error("metadata is null");
+  if (process.argv.length < 4) {
+    console.error(
+      `Usage: ${process.argv[0]} ${process.argv[1]} <PrevProposer> <Logger>`,
+    );
+    throw Error("Argument missing");
   }
 
-  for (
-    let blocknum = metadata.minBlocknum;
-    blocknum < metadata.maxBlocknum + 1;
-    blocknum++
-  ) {
-    const ret = await getProposerLog(blocknum);
+  const proposer = process.argv[2];
+  const logger = process.argv[3];
+  const blockNums: number[] = await VrankLog.distinct("blocknum", {
+    proposer: proposer,
+  });
 
-    // if no log exist, consider it late (problematic).
-    const numLates = ret?.assessment?.lateTimes.filter((x) => x >= 300);
-    if (numLates == null) {
+  for (const blocknum of blockNums) {
+    const log = await VrankLog.findOne({
+      $and: [{ blocknum: blocknum + 1 }, { logger: logger }],
+    });
+    if (log == null) {
+      console.error(`Log at ${blocknum + 1} not found. continuing...`);
+      continue;
+    }
+    if (log?.assessment?.lateTimes == null) {
       continue;
     }
 
-    if (numLates.length > threshold) {
-      console.log({ blocknum, numLates });
+    const over300 = [];
+    for (let i = 0; i < log.assessment.lateTimes.length; i++) {
+      if (log.assessment.lateTimes[i] >= 300) {
+        over300.push(log.assessment.late[i]);
+      }
+    }
+    if (over300.length > 1) {
+      console.log(`block: ${log.blocknum}, Over 300ms: ${over300}`);
     }
   }
 
   await mongoose.disconnect();
-}
-
-async function getProposerLog(blocknum: number) {
-  return await VrankLog.findOne({
-    $and: [
-      { blocknum: blocknum },
-      {
-        $where: function () {
-          return this.logger == this.proposer;
-        },
-      },
-    ],
-  });
 }
 
 // We recommend this pattern to be able to use async/await everywhere
