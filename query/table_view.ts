5161cc6952f7b3aa2af3c2f98e87710f3a20da1c
Chihyun Song <ian0371@gmail.com>
2023-10-18 10:27:32 +0900

    Output in JSON

--- query/table_view.ts
+++ query/table_view.ts
@@ -2,16 +2,20 @@ import * as mongoose from "mongoose";
 import { blockView, getBlockNumsFromArgs, csv } from "./util";
 
 async function main() {
-  console.log("Connecting Mongo DB...");
   await mongoose.connect("mongodb://127.0.0.1:27017/vrank");
-  console.log("Connected successfully");
 
   const blockNumList = await getBlockNumsFromArgs();
 
   for (const num of blockNumList) {
     const record = await blockView(num);
     const info = `num:${num}/proposer:${record.proposer}/prev_proposer:${record.prev_proposer}`;
-    console.log(csv(info, record.map));
+    const output = {
+      num,
+      proposer: record.proposer,
+      prev_proposer: record.prev_proposer,
+      csv: csv(info, record.map),
+    };
+    console.log(JSON.stringify(output));
   }
 
   await mongoose.disconnect();
